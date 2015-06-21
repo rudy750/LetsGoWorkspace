a1ccbc855b545fd8201558b2166be1d5bd0ce09e
rudy750 <rudy750@gmail.com>
2015-06-21 00:07:05 -0400

    Added values and variables

--- src/github.com/rudy750/hello/hello.go
+++ src/github.com/rudy750/hello/hello.go
@@ -4,6 +4,6 @@ import ("fmt"
         "github.com/rudy750/stringutil"
         )
 func main() {
-  fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+  fmt.Printf(stringutil.Reverse("!oG ,olleH\n"))
 
 }
